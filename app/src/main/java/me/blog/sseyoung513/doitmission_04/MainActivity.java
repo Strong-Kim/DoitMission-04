15195679392705faad9b5a2a364a2c8670d76099
strong_kim <sseyoung513@naver.com>
2017-08-22 18:30:16 +0900

    Initial commit

---
+++ app/src/main/java/me/blog/sseyoung513/doitmission_04/MainActivity.java
@@ -0,0 +1,13 @@
+package me.blog.sseyoung513.doitmission_04;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
